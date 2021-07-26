10b0dc610bff5cdca5b10a3243b2fc5ee51ea85f
jack <jack0485@foxmail.com>
2021-07-26 16:52:08 +0800

    位图模块编写

--- bitfield.c
+++ bitfield.c
@@ -1,1 +1,193 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "parse_metafile.h"
 #include "bitfield.h"
+
+extern int pieces_length;
+extern char *file_name;
+
+Bitmap *bitmap = NULL;          // 指向位图
+int download_piece_num = 0;     // 当前已下载的piece数
+
+int create_bitfield() {
+    bitmap = (Bitmap *)malloc(sizeof(Bitmap));
+    if (bitmap == NULL) {
+        printf("allocate memory for bitmap failed\n");
+        return -1;
+    }
+
+    // pieces_length除以20即为总的piece数
+    bitmap->valid_length = pieces_length / 20;
+    bitmap->bitfield_length = pieces_length / 20 / 8;
+    if ((pieces_length/20)%8 != 0) bitmap->bitfield_length++;
+
+    bitmap->bitfield = (unsigned char *)malloc(bitmap->bitfield_length);
+    if (bitmap->bitfield == NULL) {
+        printf("allocate memory for bitmap->bitfield failed\n");
+        if (bitmap != NULL) free(bitmap);
+        return -1;
+    }
+
+    char bitmapfile[64];
+    sprintf(bitmapfile, "%dbitmap", pieces_length);
+    int i;
+    FILE *fp = fopen(bitmapfile, "rb");
+    if (fp == NULL) {   // 若文件打开失败，说明开始的是一个全新的下载
+        memset(bitmap->bitfield, 0, bitmap->bitfield_length);
+    } else {
+        fseek(fp, 0, SEEK_SET);
+        for (i = 0; i < bitmap->bitfield_length; i++)
+            (bitmap->bitfield)[i] = fgetc(fp);
+        fclose(fp);
+        // 给download_piece_num赋新的初值
+        download_piece_num = get_download_piece_num();
+    }
+
+    return 0;
+}
+
+int get_bit_value(Bitmap *bitmap, int index) {
+    int ret;
+    int byte_index;
+    unsigned char byte_value;
+    unsigned char inner_byte_index;
+
+    if (bitmap == NULL || bitmap->bitfield == NULL) return -1;
+    if (index >= bitmap->valid_length || index < 0) return -1;
+    // 获取字节下标
+    byte_index = index / 8;
+    // 根据下标获取对应字节值
+    byte_value = bitmap->bitfield[byte_index];
+    // 字节位偏移
+    inner_byte_index = index % 8;
+    // 字节位右移
+    byte_value = byte_value >> (8 - inner_byte_index);
+    // 判断是否为偶数，是则最低位必为0，否则为1，也即index对应的位为0或1
+    if (byte_value % 2 == 0) ret = 0;
+    else ret = 1;
+
+    return ret;
+}
+
+int set_bit_value(Bitmap *bitmap, int index, unsigned char value) {
+    int byte_index;
+    unsigned char inner_byte_index;
+
+    if (bitmap == NULL || bitmap->bitfield == NULL) return -1;
+    if (index >= bitmap->valid_length || index < 0) return -1;
+    byte_index = index / 8;
+    inner_byte_index = index % 8;
+    switch (value) {
+        case 0:
+            bitmap->bitfield[byte_index] &= ~(1U << (8 - inner_byte_index));
+            return 0;
+        case 1:
+            bitmap->bitfield[byte_index] |= (1U << (8 - inner_byte_index));
+            return 0;
+        default:
+            printf("value not 0 or 1\n");
+            return -1;
+    }
+}
+
+int all_zero(Bitmap *bitmap) {
+    if (bitmap->bitfield == NULL) return -1;
+
+    memset(bitmap->bitfield, 0, bitmap->bitfield_length);
+    return 0;
+}
+
+int all_set(Bitmap *bitmap) {
+    if (bitmap->bitfield == NULL) return -1;
+
+    memset(bitmap->bitfield, 0xff, bitmap->bitfield_length);
+    return 0;
+}
+
+void release_memory_in_bitfield() {
+    if (bitmap->bitfield != NULL) free(bitmap->bitfield);
+    if (bitmap != NULL) free(bitmap);
+}
+
+int print_bitfield(Bitmap *bitmap) {
+    int num = 0;
+    if (bitmap == NULL || bitmap->bitfield == NULL) return -1;
+
+    for (int i = 0; i < bitmap->valid_length; ++i) {
+        get_bit_value(bitmap, i) ? printf("* "):printf(" ");
+        if (i+1 % 20 == 0) printf("\n");
+    }
+
+    return 0;
+}
+
+int restore_bitmap() {
+    int fd;
+    char bitmapfile[64];
+
+    if ( (bitmap == NULL) || (bitmap->bitfield == NULL) || (file_name == NULL)) return -1;
+    sprintf(bitmapfile, "%dbitmap", pieces_length);
+    fd = open(bitmapfile, O_RDWR|O_CREAT|O_TRUNC, 0666);
+    if (fd < 0) { printf("bitmapfile open failed\n"); return -1; }
+    write(fd, bitmap->bitfield, bitmap->bitfield_length);
+    close(fd);
+
+    return 0;
+}
+
+int is_interested(Bitmap *dst, Bitmap *src) {
+    unsigned char const_char[8] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+    unsigned char c1, c2;
+    int i, j;
+
+    if (dst == NULL || src == NULL) return -1;
+    if (dst->bitfield == NULL || src->bitfield == NULL) return -1;
+    if (dst->bitfield_length != src->bitfield_length || dst->valid_length != src->valid_length) return -1;
+
+    // 如果dst中某位为1而src对应为0，则说明src对dst感兴趣
+    for (i = 0; i < dst->bitfield_length-1; i++) {
+        for (j = 0; j < 8; j++) {   // 比较某个字节的所有位
+            c1 = (dst->bitfield)[i] & const_char[j];    // 获取每一位的值
+            c2 = (src->bitfield)[i] & const_char[j];
+            if (c1 > 0 && c2 == 0) return 1;
+        }
+    }
+
+    // 位图的valid_length可能小于bitfield_length*8，也就是最后一个字节有效位数可能不等，
+    // 需要单独比较
+    j = dst->valid_length % 8;
+    c1 = dst->bitfield[dst->bitfield_length-1];
+    c2 = src->bitfield[src->bitfield_length-1];
+    for (i = 0; i < j; i++) {   // 比较位图的最后一个字节
+        if ((c1&const_char[i]) > 0 && (c2&const_char[i]) == 0)
+            return 1;
+    }
+    return 0;
+}
+
+int get_download_piece_num() {
+    unsigned char const_char[8] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
+    int i, j;
+
+    if (bitmap == NULL || bitmap->bitfield == NULL) return -1;
+    download_piece_num = 0;
+
+    for (i = 0; i < bitmap->bitfield_length-1; i++) {
+        for (j = 0; j < 8; j++) {
+            if (((bitmap->bitfield)[i] & const_char[j]) != 0)
+                download_piece_num++;
+        }
+    }
+
+    unsigned char c = (bitmap->bitfield)[i];    // c存放位图最后一个字节的值
+    j = bitmap->valid_length % 8;               // j是位图最后一个字节的有效位数
+    for (i = 0; i < j; i++) {
+        if ( (c & const_char[i]) != 0) download_piece_num++;
+    }
+    return download_piece_num;
+}
--- bitfield.h
+++ bitfield.h
@@ -7,19 +7,49 @@ typedef struct _Bitmap {
     int valid_length;               // 位图有效的总位数，每一位代表一个piece
 } Bitmap;
 
-int create_bitfield();                          // 创建位图，分配内存并进行初始化
-int get_bit_value(Bitmap *bitmap, int index);   // 获取某一位的值
-int set_bit_value(Bitmap *bitmap, int index, unsigned char value);  // 设置某一位的值
+/*功能：创建位图，分配内存并进行初始化
+ * 返回值：成功返回0，出错返回-1*/
+int create_bitfield();
 
-int all_zero(Bitmap *bitmap);                                       // 全部清零
-int all_set(Bitmap *bitmap);                                        // 全部设置为1
-void release_memory_in_bitfield();                                  // 释放bitfield.c中动态分配的内存
-int print_bitfield(Bitmap *bitmap);                                 // 打印位图值，用于测试
+/*功能：获取位图中某一位的值
+ * 参数：bitmap位图指针 index某一位下标
+ * 返回值：成功返回下标值，出错返回-1*/
+int get_bit_value(Bitmap *bitmap, int index);
 
-int restore_bitmap();                                               // 将位图存储到文件中，在下次下载时，先读取该文件
-                                                                    // 获取已经下载的进度
-int is_interested(Bitmap *dst, Bitmap *src);                        // 拥有位图src的peer是否对拥有
-                                                                    // dst位图的peer感兴趣
-int get_download_piece_num();                                       // 获取当前已下载到的总piece数
+/*功能：设置位中某一位的值
+ * 参数：bitmap位图指针 index某一位下标 value为0或者1
+ * 返回值：成功返回0，出错返回-1*/
+int set_bit_value(Bitmap *bitmap, int index, unsigned char value);
+
+/*功能：将位图所有位清0
+ * 参数：@bitmap位图指针
+ * 返回值：成功返回0，出错返回-1*/
+int all_zero(Bitmap *bitmap);
+
+/*功能：将位图所有位置1
+ * 参数：bitmap位图指针
+ * 返回值：成功返回0，出错返回-1*/
+int all_set(Bitmap *bitmap);
+
+/*功能：释放bitfield.c中动态分配的内存*/
+void release_memory_in_bitfield();
+
+/*功能：打印位图值，用于测试
+ * 参数：bitmap位图指针
+ * 返回值：成功返回0，失败返回-1*/
+int print_bitfield(Bitmap *bitmap);
+
+/*功能：位图存储到文件中，在下次下载时，先读取该文件获取已经下载的进度*
+ * 返回值：成功返回0，失败返回-1*/
+int restore_bitmap();
+
+/*功能：拥有位图src的peer是否对拥有dst位图的peer感兴趣
+ * 参数：dst目标位图 src源位图
+ * 返回值：感兴趣返回1，否则返回0*/
+int is_interested(Bitmap *dst, Bitmap *src);
+
+/*功能：获取当前已下载到的总piece数
+ * 返回值：成功返回已下载piece数，出错返回-1*/
+int get_download_piece_num();
 
 #endif //BTMETEOR_BITFIELD_H
